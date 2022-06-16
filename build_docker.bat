4d6a50165baac01b3abf3df8059277dd538e245f
Tailslide <5757188+Tailslide@users.noreply.github.com>
2022-06-16 01:13:54 -0600

    Add Docker build

--- build_docker.bat
+++ build_docker.bat
@@ -1,5 +1,7 @@
 .\venv\scripts\pip.exe freeze >> requirements.txt
-docker build --tag book-downloader .
+docker build --tag tailslide/book-downloader .
 echo running built container
 pause
-docker run --env-file=.\docker_test_env book-downloader
+docker run --env-file=.\docker_test_env tailslide/book-downloader
+echo pushing to dockerhub
+docker push tailslide/book-downloader:latest
