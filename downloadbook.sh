47dd2218a8f0a41f5d9a0c48c153e807cf2d7f70
Tailslide <5757188+Tailslide@users.noreply.github.com>
2022-09-07 10:33:15 -0600

    try to kill old image

--- downloadbook.sh
+++ downloadbook.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo Downloading $1
 CONTAINER_NAME=$(uuidgen)
-#docker run --rm -t -v /volume1/docker/ircbooks:/ircbooks -e "SEARCH_TERM=$1" -e "NICK=Tailslide" -e "LOCAL_TEMP_FOLDER=/ircbooks" --pull always --net=container:wireguard-qbittorrent_vpn_1  tailslide/book-downloader:latest
+docker run --rm -t -v /volume1/docker/ircbooks:/ircbooks -e "SEARCH_TERM=$1" -e "NICK=Tailslide" -e "LOCAL_TEMP_FOLDER=/ircbooks" --pull always --net=container:wireguard-qbittorrent_vpn_1  tailslide/book-downloader:latest
 #(sleep 28800 ; sudo docker stop $CONTAINER_NAME)& docker run  --rm --name $CONTAINER_NAME --pull=always --label=com.centurylinklabs.watchtower.enable=false  --env-file excludarr.env haijeploeg/excludarr:latest radarr exclude -a delete -y -e
 
-(sleep 28800 ; sudo docker stop $CONTAINER_NAME)& docker run --rm  --name $CONTAINER_NAME -t -v /volume1/docker/ircbooks:/ircbooks -e "SEARCH_TERM=$1" -e "NICK=Tailslide" -e "LOCAL_TEMP_FOLDER=/ircbooks" --pull always --label=com.centurylinklabs.watchtower.enable=false --net=container:wireguard-qbittorrent_vpn_1  tailslide/book-downloader:latest
+#(sleep 28800 ; sudo docker stop $CONTAINER_NAME)& docker run --rm  --name $CONTAINER_NAME -t -v /volume1/docker/ircbooks:/ircbooks -e "SEARCH_TERM=$1" -e "NICK=Tailslide" -e "LOCAL_TEMP_FOLDER=/ircbooks" --pull always --label=com.centurylinklabs.watchtower.enable=false --net=container:wireguard-qbittorrent_vpn_1  tailslide/book-downloader:latest
--- downloadnextbook.sh
+++ downloadnextbook.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 nextbook=`./findnextbook.sh`
 echo searching for $nextbook
+docker stop "$(sudo docker ps -q --filter ancestor="tailslide/book-downloader:latest" )"
 res=`./downloadbook.sh "$nextbook"`
 echo $res
 
